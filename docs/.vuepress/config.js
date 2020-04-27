3e77583bd80fc6634e5bda943ee10dca5d2b5004
Hewie8023 <Hewie8023@hotmail.com>
2020-04-28 01:50:26 +0800

    feat: add lineNumber

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,7 @@
 const headConfig = require("./config/headConfig");
 const pluginsConfig = require("./config/pluginsConfig");
 const navConfig = require("./nav");
-const secret = require('./config/secret');
+const secret = require("./config/secret");
 
 module.exports = {
   title: "折腾的人生",
@@ -12,29 +12,34 @@ module.exports = {
   plugins: pluginsConfig,
   themeConfig: {
     type: "blog",
-    authorAvatar: '/avatar.png',
-    author: 'Hewie',
+    authorAvatar: "/avatar.png",
+    author: "Hewie",
     lastUpdated: "更新时间",
     logo: "/assets/img/logo.png",
     nav: navConfig,
     valineConfig: {
       appId: secret.appId, // your appId
       appKey: secret.appKey, // your appKey
+      visitor: true,
     },
+
     friendLink: [
       {
-        title: 'Hewie博客',
-        desc: 'Hewie个人博客',
-        email: '349545547@qq.com',
-        link: 'http://www.rxwcv.cn/'
+        title: "Hewie博客",
+        desc: "Hewie个人博客",
+        email: "349545547@qq.com",
+        link: "http://www.rxwcv.cn/",
       },
     ],
-    startYear: '2019',
+    startYear: "2019",
     blogConfig: {
       tag: {
-        location: 3,     // 在导航栏菜单中所占的位置，默认3
-        text: 'Tag'      // 默认文案 “标签”
-      }
-    }
+        location: 3, // 在导航栏菜单中所占的位置，默认3
+        text: "Tag", // 默认文案 “标签”
+      },
+    },
+  },
+  markdown: {
+    lineNumbers: true,
   },
 };
--- docs/.vuepress/config/secret.js
+++ docs/.vuepress/config/secret.js
@@ -1,5 +1,7 @@
 module.exports = {
     appId: "cEAhq1ldjpXi0Ut4Vb3wWMeJ-gzGzoHsz", // your appId
     appKey: "Ibrk1zQi4pHhxKzNWULrC4gj", // your appKey
-    'ga': 'UA-149900822-2'
+    'ga': 'UA-149900822-2',
+    clientId:"0669ba4581cb9850a6cd",
+    clientSecret:"07df9641e2b2de97251cd2a27fdaf741b18dbdd3"
 }
